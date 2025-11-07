82f89d5218abf584b5d548fce033e370ac8c1e69
NooraAlsaiari077 <445200055@student.ksu.edu.sa>
2025-11-07 19:07:10 +0300

    Refactor Product class to include getReviews method

--- ECommerceManagmentSystem/src/main/Product.java
+++ ECommerceManagmentSystem/src/main/Product.java
@@ -1,13 +1,10 @@
 package main;
 
 public class Product {
-
     private String id;
     private String name;
     private double price;
     private int stock;
-
-    // List of reviews linked to this product
     private LinkedList<Review> reviews = new LinkedList<>();
 
     public Product(String id, String name, double price, int stock) {
@@ -17,45 +14,64 @@ public class Product {
         this.stock = stock;
     }
 
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getStock() { return stock; }
-
-    public void setPrice(double price) { this.price = price; }
-    public void setStock(int stock) { this.stock = stock; }
 
     public void addReview(Review r) {
         reviews.insert(r);
     }
 
-    // Used in getAverageRating() in DataHandle
+
     public double getAverageRating() {
+    	if (reviews.empty()) 
+    		return 0.0;
+    	
+    	double sum = 0;
+    	int count = 0;
+    	reviews.findFirst();
+    	while (!reviews.last()) {
+    		sum += reviews.retrieve().getRating();
+    		reviews.findNext();
+    		count++;
+    	}
+    	
+    	sum += reviews.retrieve().getRating();
+    	count++;
 
-    if (reviews.empty()) 
-        return 0.0;
-
-    double sum = 0;
-    int count = 0;
-
-    reviews.findFirst();
-    while (!reviews.last()) {
-        sum += reviews.retrieve().getRating();
-        reviews.findNext();
-        count++;
+    	return sum / count;
     }
 
-    // for last element 
-    sum += reviews.retrieve().getRating();
-    count++;
 
-    return sum / count;
-}
+    public String getId() { 
+    	return id; 
+    	}
+    
+    public String getName() { 
+    	return name; 
+    	}
+    
+    public double getPrice() { 
+    	return price; 
+    	}
+    
+    public int getStock() { 
+    	return stock; 
+    	}
+    
+    public LinkedList<Review> getReviews() { 
+    	return reviews; 
+    	}
 
-
+    public void setPrice(double price) { 
+    	this.price = price; 
+    	}
+    
+    public void setStock(int stock) { 
+    	this.stock = stock; 
+    	}
+    
     @Override
     public String toString() {
         return "Product{ ID=" + id + ", Name=" + name +
                 ", Price=" + price + ", Stock=" + stock + " }";
     }
 }
+
